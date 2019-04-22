a5dd5133b1abf6aa0b43cf849f76aa88f4a7f7fe
Anthony King <cybojenix@slimroms.org>
2019-04-22 15:41:12 +0100

    add a way to retrieve commands from the registry

--- src/transport/webusb/commands/registry.ts
+++ src/transport/webusb/commands/registry.ts
@@ -1,8 +1,13 @@
 import { Command } from './interface';
 
+type defaultArg0Name = 'default';
+type storedArg0 = defaultArg0Name | number;
+const defaultArg0Name: defaultArg0Name = 'default';
+
+
 interface Store {
   [commandName: string]: {
-    default: Command;
+    [defaultArg0Name]: Command;
     [arg0: number]: Command;
   };
 }
@@ -31,11 +36,27 @@ class Registry {
     );
   }
 
+  public retrieve(
+    { commandName, arg0 }: { commandName: string; arg0?: number },
+  ): Command {
+    const commands = this.store[commandName];
+    if (!commands) throw Error(`Command not found: ${commandName}`);
+
+    const storedArg0 = Registry.constructStoredArg0(arg0);
+    const command = commands[storedArg0] || commands[defaultArg0Name];
+    if (!command) throw Error(`Command not found: {commandName: ${commandName}, arg0: ${arg0}`);
+    return command;
+  }
+
+  private static constructStoredArg0(arg0?: number): storedArg0 {
+    return (arg0 === undefined) ? defaultArg0Name : arg0;
+  }
+
   private storeCommand<T extends Command>(
     { commandName, arg0 }: { commandName: string; arg0?: number },
     command: T,
   ): T {
-    const storedArg0: 'default' | number = (arg0 === undefined) ? 'default' : arg0;
+    const storedArg0 = Registry.constructStoredArg0(arg0);
     this.store[commandName][storedArg0] = command;
     return command;
   }
